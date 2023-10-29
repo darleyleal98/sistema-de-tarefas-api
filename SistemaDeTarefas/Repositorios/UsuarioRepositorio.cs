a5b0df0b671ce676468260a252365e6aecb19ac0
Darley Leal <darleyleal98@gmail.com>
2023-10-29 07:27:56 -0300

    Criada nova interface CRUD

---
+++ SistemaDeTarefas/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SistemaDeTarefas.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        public Task<UsuarioModel> BuscarPorId(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UsuarioModel>> BuscarTodosOsUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<UsuarioModel> AdicionarUsuario(UsuarioModel usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
